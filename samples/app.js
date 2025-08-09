ae51a314aa6b8ae0563c89aa1256b1ac4c457e39
Krishna Agarwal <kr1shna4garwal@gmail.com>
2025-08-09 14:28:24 +0530

    💩: Remove deprecated files and schemas for code cleanup

--- samples/app.js
+++
@@ -1,8 +0,0 @@
-// app.js
-
-function getUser() {
-  // FIXME: This endpoint is deprecated, use /v2/user instead
-  fetch('/api/user')
-    .then(res => res.json())
-    .then(data => console.log(data));
-}
--- samples/main.py
+++
@@ -1,9 +0,0 @@
-# main.py
-
-# TODO: Refactor this function to be more efficient
-def calculate_sum(a, b):
-    # This is a dummy secret for testing purposes
-    aws_access_key = "AKIAIOSFODNN7EXAMPLE"
-    return a + b
-
-print("Hello from Python!")
